c14625d81de989cddce0b70520ef65a41ed6a21a
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-10-14 18:41:40 -0500

    Track by bed not by room

--- app/Bed.php
+++ app/Bed.php
@@ -1,17 +1,18 @@
 <?php
 require_once("Hostel.php");
-class Room {
+class Bed {
    protected $hostel;
+   protected $bed_number;
    protected $room_number;
    protected $number_beds;
    protected $rate;
    protected $availablity = true;
 
-   public function book_room() {
+   public function book_bed() {
       $availability = false;
    }
 
-   public function free_room() {
+   public function free_bed() {
       $availability = true;
    }
 
