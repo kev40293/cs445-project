6960f692f1cb79a55c47f6c7d5957b7c4bce41eb
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-10-21 20:05:39 -0500

    Updated Bed class and unit tests for it, rest of tests broken

--- app/Bed.php
+++ app/Bed.php
@@ -3,22 +3,35 @@ require_once("Hostel.php");
 class Bed {
    protected $room_number;
    protected $rate;
-   protected $isAvailable = true;
+   protected $availabilities;
 
-   public function __construct($room_num) {
+   public function __construct($room_num, $dates) {
       $this->room_number = $room_num;
+      foreach ($dates as $date) {
+         $this->availabilities[$date] = true;
+      }
    }
 
-   public function book() {
-      $this->isAvailable = false;
+   public function book($date) {
+      $this->availabilities[$date] = false;
    }
 
-   public function free() {
-      $this->isAvailable = true;
+   public function free($date) {
+      $this->availabilities[$date] = true;
    }
 
-   public function is_free() {
-      return $this->isAvailable;
+   public function free_dates() {
+      $free = array();
+      foreach ($this->availabilities as $date => $status) {
+         if ($status) {
+            $free[] = $date;
+         }
+      }
+      return $free;
+   }
+
+   public function is_free($date) {
+      return $this->availabilities[$date];
    }
 
 }
--- tests/BedTest.php
+++ tests/BedTest.php
@@ -5,23 +5,28 @@ require_once("../app/Bed.php");
 
 class BedFunctionsTest extends PHPUnit_Framework_TestCase {
    protected $bed;
+   protected $init_avail = array("10102013");
    public function setUp(){
-      $this->bed = new Bed(1);
+      $this->bed = new Bed(1, $this->init_avail);
    }
 
    public function testFree() {
-      $this->assertTrue($this->bed->is_free());
+      $avail = $this->bed->free_dates();
+      $this->assertGreaterThan(0, sizeof($avail));
+      $this->assertTrue($this->bed->is_free("10102013"));
    }
 
    public function testBooking() {
-      $this->bed->book();
-      $this->assertFalse($this->bed->is_free());
+      $numavail = sizeof($this->bed->free_dates());
+      $this->bed->book("10102013");
+      $this->assertFalse($this->bed->is_free("10102013"));
+      $this->assertEquals($numavail - 1, sizeof($this->bed->free_dates()));
    }
 
    public function testFreeBed() {
-      $this->bed->book();
-      $this->bed->free();
-      $this->assertTrue($this->bed->is_free());
+      $this->bed->book("10102013");
+      $this->bed->free("10102013");
+      $this->assertTrue($this->bed->is_free("10102013"));
    }
 }
 
