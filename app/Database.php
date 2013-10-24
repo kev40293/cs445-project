37fb16254bafed3925209615ee9b5ed961a08457
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-10-23 21:54:01 -0500

    Added some additional functions for the DB to manage

--- app/Database.php
+++ app/Database.php
@@ -3,6 +3,9 @@
 interface DatabaseInterface {
    public function search_beds($parameters);
    public function search_reservations($parameters);
+   public function record_reservation($resv);
+   public function delete_reservation($resv);
+   public function add_customer($cust);
 }
 
 require_once("Bed.php");
@@ -10,13 +13,32 @@ require_once("Bed.php");
 class MemoryDatabase implements DatabaseInterface {
    protected $beds;
    protected $reservations;
+   protected $customer;
    public function __construct($init_beds) {
       $this->beds = $init_beds;
+      $this->reservations = array();
+      $this->customers = array();
    }
    public function search_beds($parameters) {
       return null;
    }
+
    public function search_reservations($parameters){
+   }
+
+   public function record_reservation($resv){
+      $this->reservations[] = $resv;
+   }
+   public function delete_reservation($resv) {
+      $new_arr = array();
+      foreach ($this->reservation as $r) {
+         if ($r != $resv)
+            $new_arr[] = $r;
+      }
+      $this->reservations = $new_arr;
+   }
+   public function add_customer($cust){
+      $this->customers[] = $cust;
    }
 }
 
--- tests/MemDBTest.php
+++ tests/MemDBTest.php
@@ -4,11 +4,14 @@ require_once ("../app/Database.php");
 require_once ("PHPUnit.php");
 class MemDBTest extends PHPUnit_Framework_TestCase {
    protected $db;
-   protected function setup() {
+   protected function setUp() {
       $this->db = new MemoryDatabase(array());
    }
 
    public function testCreate() {
       $this->assertInstanceOf('MemoryDatabase', $this->db);
    }
+
 }
+
+?>
