8024efd5592175d7930198cca3d6a0e9ed73e063
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-10-28 19:53:18 -0500

    added some comments and provided framework for search

--- app/Database.php
+++ app/Database.php
@@ -9,21 +9,39 @@ interface DatabaseInterface {
 }
 
 require_once("Bed.php");
+require_once("Hostel.php");
+require_once("Reservation.php");
 
 class MemoryDatabase implements DatabaseInterface {
+   protected $hostels;
    protected $beds;
    protected $reservations;
    protected $customer;
+
+   // fix the constructor to read from the XML file
    public function __construct($init_beds) {
       $this->beds = $init_beds;
       $this->reservations = array();
       $this->customers = array();
    }
+   // City, start date, end date/numdays
    public function search_beds($parameters) {
-      return null;
+      $hst = array();
+      $results = array();
+      if (exists($parameters["city"])) {
+         // get appropriate hostel
+      }
+      else {
+         $hst = $this->hostels;
+      }
+      foreach ($hst as $host) {
+         // append available beds
+      }
+      return $results;
    }
 
    public function search_reservations($parameters){
+      return $this->reservations;
    }
 
    public function record_reservation($resv){
