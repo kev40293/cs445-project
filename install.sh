41d9d0334a245cfe9ab1f6f05d3080889e42824f
Kevin Brandstatter <kbrandst@hawk.iit.edu>
2013-11-16 21:32:29 -0600

    install script also configures apache

--- install.sh
+++ install.sh
@@ -17,3 +17,11 @@ apt-get install phpunit
 pear update-channels
 pear isntall PHP_CodeCoverage
 pear install phpunit
+
+############################
+# Configure and install    #
+# the apache configuration #
+############################
+DIRNAME=$(readlink -f .)/htdocs
+sed -e "s|DOCUMENTROOT|$DIRNAME|" apache-web.conf > /etc/apache2/sites-available/default
+service apache2 reload
